d2afe5c4a8039e1f484373505d09f7930d4ca82e
BZaldua <benatzaldua1775@gmail.com>
2026-07-21 12:14:53 +0200

    add exception API exception handlers

---
+++ app/api/exceptions.py
@@ -0,0 +1,23 @@
+import logging
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from app.exceptions import AppModuleException
+
+logger = logging.getLogger("app")
+
+
+def app_module_exception_handler(request: Request, exc: AppModuleException):
+    logger.warning(
+        f"Error {exc.status_code} en {request.method} {request.url.path} -> {exc.message}"
+    )
+
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message, "error_code": exc.__class__.__name__},
+    )
+
+
+def init_exception_handlers(app: FastAPI) -> None:
+    app.add_exception_handler(AppModuleException, app_module_exception_handler)
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from dishka.integrations.fastapi import setup_dishka
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from app.api.exceptions import init_exception_handlers
 from app.api.v1.asset_router import router as asset_router
 from app.api.v1.task_router import router as task_router
 from app.container import AppProvider
@@ -17,6 +18,7 @@ app = FastAPI(
 
 app.include_router(asset_router, prefix="/api/v1", tags=["Assets"])
 app.include_router(task_router, prefix="/api/v1", tags=["Tasks"])
+init_exception_handlers(app)
 
 container = make_async_container(AppProvider())
 setup_dishka(container, app)
