d0ef8c1a29215bbcf0eb92c5e14ca1c10a4b8c13
BZaldua <benatzaldua1775@gmail.com>
2026-07-21 11:59:57 +0200

    add exception classes

---
+++ app/exceptions.py
@@ -0,0 +1,28 @@
+class AppModuleException(Exception):
+    """Base exception class for all app exceptions"""
+
+    status_code: int = 500
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+
+class NotFoundException(AppModuleException):
+    """Base exception class for not found exceptions"""
+
+    status_code = 404
+
+
+class AssetNotFoundException(NotFoundException):
+    """Business exception when Asset does not exist"""
+
+    def __init__(self, asset_id: int):
+        super().__init__(f"Asset ID={asset_id} does not exist")
+
+
+class TaskNotFoundException(NotFoundException):
+    """Business exception when Task does not exist"""
+
+    def __init__(self, task_id: int):
+        super().__init__(f"Task ID={task_id} does not exist")
