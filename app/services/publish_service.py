87feda89dd0c8b9fd253bd698e564c2cba9e29f0
BZaldua <benatzaldua1775@gmail.com>
2026-07-22 17:36:38 +0200

    add file to minio

--- app/services/publish_service.py
+++ app/services/publish_service.py
@@ -1,6 +1,6 @@
 from app.domain import Publish
 from app.infrastructure.database import PublishModel
-from app.infrastructure.repositories import PublishRepository
+from app.infrastructure.repositories import MinioRepository, PublishRepository
 
 from .asset_service import AssetService
 from .task_service import TaskService
@@ -12,10 +12,12 @@ class PublishService:
         publish_repo: PublishRepository,
         asset_service: AssetService,
         task_service: TaskService,
+        minio_repository: MinioRepository,
     ):
         self.repository = publish_repo
         self.asset_service = asset_service
         self.task_service = task_service
+        self.minio_repository = minio_repository
 
     async def create(self, publish: Publish) -> Publish:
         task = await self.task_service.get_by_id(publish.task.id)
@@ -23,12 +25,22 @@ class PublishService:
 
         latest_version = await self.repository.get_latest_version(asset.id, task.id)
 
+        sanitized_filename = publish.file_input.filename.replace(" ", "_").lower()
+        object_name = f"{asset.name}/{task.name}/{sanitized_filename}_v{latest_version}"
+
+        fs_path = self.minio_repository.save(
+            object_name=object_name,
+            content_type=publish.file_input.content_type,
+            length=publish.file_input.size,
+            data=publish.file_input.content,
+        )
+
         publish = PublishModel(
             asset_id=asset.id,
             task_id=task.id,
             version=latest_version + 1,
             author=publish.author,
-            fs_path="PATH",  # TODO: set a valid path
+            fs_path=fs_path,
         )
 
         published_model: PublishModel = await self.repository.add(publish)
--- tests/services/test_publish_service.py
+++ tests/services/test_publish_service.py
@@ -1,8 +1,9 @@
+import io
 from unittest.mock import AsyncMock, MagicMock
 
 import pytest
 
-from app.domain import Asset, Publish, Task
+from app.domain import Asset, FileInput, Publish, Task
 from app.infrastructure.database import PublishModel
 from app.services.publish_service import PublishService
 
@@ -12,6 +13,13 @@ def mock_publish_repo():
     repo = MagicMock()
     repo.get_latest_version = AsyncMock()
     repo.add = AsyncMock()
+    return repo
+
+
+@pytest.fixture
+def mock_minio_repo():
+    repo = MagicMock()
+    repo.save = MagicMock()
     return repo
 
 
@@ -30,20 +38,37 @@ def mock_task_service():
 
 
 @pytest.fixture
-def publish_service(mock_publish_repo, mock_asset_service, mock_task_service):
+def publish_service(
+    mock_publish_repo, mock_asset_service, mock_task_service, mock_minio_repo
+):
     return PublishService(
         publish_repo=mock_publish_repo,
         asset_service=mock_asset_service,
         task_service=mock_task_service,
+        minio_repository=mock_minio_repo,
     )
 
 
 @pytest.mark.asyncio
 async def test_create_publish_success(
-    publish_service, mock_publish_repo, mock_asset_service, mock_task_service
+    publish_service,
+    mock_publish_repo,
+    mock_asset_service,
+    mock_task_service,
+    mock_minio_repo,
 ):
     # Arrange
-    request = Publish(asset=Asset(id=1), task=Task(id=10), author="John Doe")
+    file_content = b"Test file content"
+    mock_file = FileInput(
+        filename="test_file",
+        content=io.BytesIO(file_content),
+        size=len(file_content),
+        content_type="text/plain",
+    )
+
+    request = Publish(
+        asset=Asset(id=1), task=Task(id=10), author="John Doe", file_input=mock_file
+    )
 
     mock_task = MagicMock()
     mock_task.id = 10
@@ -57,8 +82,11 @@ async def test_create_publish_success(
 
     mock_publish_repo.get_latest_version.return_value = 2
 
+    fs_file_path = f"/bucket/{mock_asset.name}/{mock_task.name}/{mock_file.filename}_3"
+    mock_minio_repo.save.return_value = fs_file_path
+
     expected_publish_model = PublishModel(
-        asset_id=1, task_id=10, version=3, author="John Doe", fs_path="PATH"
+        asset_id=1, task_id=10, version=3, author="John Doe", fs_path=fs_file_path
     )
     mock_publish_repo.add.return_value = expected_publish_model
 
@@ -69,29 +97,42 @@ async def test_create_publish_success(
     mock_task_service.get_by_id.assert_called_once_with(10)
     mock_asset_service.get_by_id.assert_called_once_with(1)
     mock_publish_repo.get_latest_version.assert_called_once_with(1, 10)
-
+    mock_minio_repo.save.asset_called_once()
     mock_publish_repo.add.assert_called_once()
     saved_publish = mock_publish_repo.add.call_args[0][0]
     assert saved_publish.asset_id == 1
     assert saved_publish.task_id == 10
     assert saved_publish.version == 3
     assert saved_publish.author == "John Doe"
-    assert saved_publish.fs_path == "PATH"
+    assert saved_publish.fs_path == fs_file_path
 
-    # 3. Verificar la respuesta DTO devuelta
     assert isinstance(result, Publish)
     assert result.asset.name == "Character_Hero"
     assert result.task.name == "Modeling"
     assert result.version == 3
-    assert result.file_path == "PATH"
+    assert result.file_path == fs_file_path
 
 
 @pytest.mark.asyncio
 async def test_create_publish_first_version(
-    publish_service, mock_publish_repo, mock_asset_service, mock_task_service
+    publish_service,
+    mock_publish_repo,
+    mock_asset_service,
+    mock_task_service,
+    mock_minio_repo,
 ):
     # Arrange
-    request = Publish(asset=Asset(id=1), task=Task(id=10), author="John Doe")
+    file_content = b"Test file content"
+    mock_file = FileInput(
+        filename="test_file",
+        content=io.BytesIO(file_content),
+        size=len(file_content),
+        content_type="text/plain",
+    )
+
+    request = Publish(
+        asset=Asset(id=1), task=Task(id=10), author="John Doe", file_input=mock_file
+    )
 
     mock_task = MagicMock()
     mock_task.id = 10
@@ -105,8 +146,11 @@ async def test_create_publish_first_version(
 
     mock_publish_repo.get_latest_version.return_value = 0
 
+    fs_file_path = f"/bucket/{mock_asset.name}/{mock_task.name}/{mock_file.filename}_1"
+    mock_minio_repo.save.return_value = fs_file_path
+
     expected_publish_model = PublishModel(
-        asset_id=1, task_id=10, version=1, author="John Doe", fs_path="PATH"
+        asset_id=1, task_id=10, version=1, author="John Doe", fs_path=fs_file_path
     )
     mock_publish_repo.add.return_value = expected_publish_model
 
@@ -114,6 +158,7 @@ async def test_create_publish_first_version(
     result = await publish_service.create(request)
 
     # Assert
+    mock_minio_repo.save.asset_called_once()
     assert result.version == 1
     saved_publish = mock_publish_repo.add.call_args[0][0]
     assert saved_publish.version == 1
