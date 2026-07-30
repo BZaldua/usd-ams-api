4c17cf7a635d8eae7645dd1e690fa62f854c0e64
Beñat Zaldua <14213853+BZaldua@users.noreply.github.com>
2026-07-30 19:08:56 +0200

    Python testing workflow (#3)
    * add python testing workflow
    
    * add install dependencies
    
    * upgrade setup-python version to v5
    
    * add no cache
    
    * add htpx dev dependency
    
    * add verbose
    
    * add annotations import
    
    * reorder classes
    
    * delete verbose
    
    * delete docs

--- app/api/v1/schemas/asset_versions_response_dto.py
+++ app/api/v1/schemas/asset_versions_response_dto.py
@@ -4,12 +4,12 @@ from .asset_create_response_dto import AssetCreateResponseDTO
 from .task_type_response_dto import TaskTypeResponseDTO
 
 
+class AssetVersionDTO(BaseModel):
+    version: int = Field(..., description="Version number")
+    author: str = Field(description="Version author name")
+
+
 class AssetVersionsResponseDTO(BaseModel):
     asset: AssetCreateResponseDTO = Field(...)
     task: TaskTypeResponseDTO = Field(...)
     versions: list[AssetVersionDTO] = Field(..., description="Existing versions")
-
-
-class AssetVersionDTO(BaseModel):
-    version: int = Field(..., description="Version number")
-    author: str = Field(description="Version author name")
