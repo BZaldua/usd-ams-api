41c6721dd2cd8ea909db80a9106005b73d952571
BZaldua <benatzaldua1775@gmail.com>
2026-08-01 17:54:33 +0200

    relocate test config file

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,17 +7,23 @@ from fastapi import FastAPI
 from httpx import ASGITransport, AsyncClient
 
 from app.api.v1.endpoints import asset_router, task_router
+from app.api.v1.mappers import TaskMapper
 from app.services import AssetService, PublishService, TaskService
 
 
 class APIV1TestProvider(Provider):
     def __init__(
-        self, task_mock: AsyncMock, asset_mock: AsyncMock, publish_mock: AsyncMock
+        self,
+        task_mock: AsyncMock,
+        asset_mock: AsyncMock,
+        publish_mock: AsyncMock,
+        task_mapper_mock: AsyncMock,
     ):
         super().__init__()
         self._task_mock = task_mock
         self._asset_mock = asset_mock
         self._publish_mock = publish_mock
+        self._task_mapper_mock = task_mapper_mock
 
     @provide(scope=Scope.REQUEST)
     def get_task_service(self) -> TaskService:
@@ -30,6 +36,10 @@ class APIV1TestProvider(Provider):
     @provide(scope=Scope.REQUEST)
     def get_publish_service(self) -> PublishService:
         return self._publish_mock
+
+    @provide(scope=Scope.APP)
+    def get_task_mapper_mock(self) -> TaskMapper:
+        return self._task_mapper_mock
 
 
 @pytest.fixture
@@ -48,14 +58,21 @@ def publish_service_mock():
 
 
 @pytest.fixture
-async def client(task_service_mock, asset_service_mock, publish_service_mock):
+def task_mapper_mock():
+    return AsyncMock(spec=TaskMapper)
+
+
+@pytest.fixture
+async def client(
+    task_service_mock, asset_service_mock, publish_service_mock, task_mapper_mock
+):
     app = FastAPI()
 
     app.include_router(task_router)
     app.include_router(asset_router)
 
     provider = APIV1TestProvider(
-        task_service_mock, asset_service_mock, publish_service_mock
+        task_service_mock, asset_service_mock, publish_service_mock, task_mapper_mock
     )
     container = make_async_container(provider)
     setup_dishka(container, app)
